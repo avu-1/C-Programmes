df5027d281a1b7a3883d9caa566f352e4d39ebe0
avu-01 <dark.moon0010001@gmail.com>
2024-06-04 00:19:36 +0530

    1

---
+++ number is dicisible by 3 and 5 but not 15.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number :");
+    scanf("%d", &n);
+    if ((n % 5 == 0) || (n % 3 == 0))
+    {
+        if (n % 15 != 0)
+        {
+            printf("The Number is Divisible by 5 or 3 but not divisible by 15");
+        }
+        else
+        {
+            printf("The Number is Divisible by 5 or 3 but also divisible by 15");
+        }
+    }
+    else
+    {
+         printf("The Number is Not Divisible by 5 or 3");
+    }
+    
+
+    return 0;
+    
+}   
+
